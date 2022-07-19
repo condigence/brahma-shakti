7879e10e41cb3b26ac168177fa9ef24c963e96dc
condigence <condigence@gmail.com>
2022-07-19 09:48:30 +0530

    deleted old project

--- bs-product-service/src/main/java/com/condigence/nsproductservice/repository/BrandRepository.java
+++
@@ -1,8 +0,0 @@
-package com.condigence.nsproductservice.repository;
-
-
-import com.condigence.nsproductservice.model.Brand;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BrandRepository extends MongoRepository<Brand, Long> {
-}
