1b612c180acdd08d1e6a33ad7517b4a3d774d5a6
condigence <condigence@gmail.com>
2022-12-30 19:26:22 +0530

    added cors origin

--- bs-service-app/src/main/java/com/condigence/controller/FavouriteController.java
+++ bs-service-app/src/main/java/com/condigence/controller/FavouriteController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin()
 @RestController
 @RequestMapping("/api/bs-favourite")
 public class FavouriteController {
---
+++ bs-service-app/src/main/java/com/condigence/utils/CustomCodes.java
@@ -0,0 +1,2 @@
+package com.condigence.utils;public class CustomCodes {
+}
